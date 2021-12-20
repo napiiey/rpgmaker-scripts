bf66abb2a4cda24fc5b21ee25c1fe3d9b67607a4
なぴぃ <88929313+napiiey@users.noreply.github.com>
2021-12-20 20:40:10 +0900

    Update add_action.js

--- add_action.js
+++ add_action.js
@@ -3,9 +3,10 @@ const enemyOrActor=1; //追加対象が敵か味方か 敵:0 味方:1
 const id=0; //追加対象ID
 const targetIndex=-2; //-2:ラストターゲット -1:ランダム 0以降:ターゲットの番号
 const skillId=31; //スキルID
-this.iterateBattler(enemyOrActor,id,function(battler){if (!battler.isDeathStateAffected()){
+this.iterateBattler(enemyOrActor,id,function(battler){if(!battler.isDeathStateAffected()){
     const action = new Game_Action(battler, true); action.setSkill(skillId);
     if (targetIndex === -2) {action.setTarget(battler._lastTargetIndex);
     } else if (targetIndex === -1) {action.decideRandomTarget();
-    } else {action.setTarget(targetIndex);};battler._actions.push(action);this.setWaitMode('action');
+    } else {action.setTarget(targetIndex);};battler._actions.push(action);
+    this.setWaitMode('action');
 }}.bind(this))
